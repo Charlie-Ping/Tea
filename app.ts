6e0c31386796e85ba53fd5824c51da7c8d3ded98
Charlie-Ping <1758489207@qq.com>
2022-04-01 21:54:57 +0800

    ..nothing

---
+++ app.ts
@@ -0,0 +1,53 @@
+/*
+* ====== priority from low to high
+*
+* 1. read music file into a variable
+* 2. range for-each notes:
+* 		1. sleep time
+* 		2. execute command
+* 		
+* 
+*
+* 1. Readers (Class):
+*       0. Note
+*       	- vol
+*       	- note
+*       	- before time
+*
+* 		1. simple music file reader
+* 		2. midi reader
+* 
+* 2. Reader Impl:
+* 		1. read notes -> Note
+* 		2. show music info
+* 		3. 
+* 		2. jump into somewhere
+* 		
+* 
+*
+*
+* 		2. web address list
+* 		3. 
+* 		1. read file
+* 		2. read file stream
+* 		3. read web stream
+* 		4. read 
+*
+* 2. A player:
+* 		1. 
+* */
+
+
+
+import * as fs from "fs"
+
+class MusicReader {
+	musicpath: string;
+
+	constructor(musicpath: string) {
+		this.musicpath = musicpath;
+		
+	}
+
+	private readMusicStream()	
+}
